d9f6aabd7e240613c2b1cc9c27cec4579049517c
MINSU1 <msong36@my.bcit.ca>
2018-03-26 12:16:48 -0700

    =ddd

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, ()=> {
+	console.log(`Server is up on the port ${port}`);
+});
